799470631868564585e62cf4f824789117bef306
goldenglorys <olusolagloryolamide@gmail.com>
2021-03-23 17:05:05 +0100

    Finished the crud ops with mongo db in a simple way-architecture-and practices

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,13 +1,27 @@
 // Directory - server/db/mongoose.js
 
+// It’s not scalable or efficient conjucting db connection in the api file. When we write unit tests for this API,
+// the unit tests are going to use their own test data(or fixtures) on their own test databases.
+// So, we want a way to be able to create connections for different purposes — some for testing
+// environments(that we can spin up and tear down at will), others for development environments, and others for production
+// environments.To do that, we’ll build a factory.
+
 const mongoose = require("mongoose");
 
 const MONGODB_URL =
-  "mongodb+srv://534L:534L@cluster0.qrzbw.mongodb.net/bookInfoDevEnvDb?retryWrites=true&w=majority";
+  "mongodb+srv://534L:11NXC4WnKjRDf7Ly@cluster0.qrzbw.mongodb.net/bookInfo?retryWrites=true&w=majority";
 
 module.exports = () =>
-  mongoose.connect(MONGODB_URL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  });
+  mongoose
+    .connect(MONGODB_URL, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    .catch((err) => {
+      console.error("MongoDB connection error:", err.message);
+    });
+
+// The benefits of making our connection as a function are that we can call that function later in
+// code to connect to the database from files aimed at production and those aimed at local and
+// remote integration testing both on - device and with a remote CI / CD pipeline / build server.
--- server/mongodbCrud.js
+++ server/mongodbCrud.js
@@ -1,5 +1,10 @@
 // Getting the module from node_modules.
 const express = require("express");
+const mongoose = require("mongoose");
+
+// Database connection and model.
+require("./db/mongoose")();
+const Book = require("./models/book.js");
 
 // This creates our Express App.
 const app = express();
@@ -14,27 +19,76 @@ app.use(express.urlencoded({ extended: true }));
 const PORT = 3000;
 
 // HTTP POST /books
-app.post("/books", (req, res) => {
-  // ...
-  console.log("A POST Request was made!");
+app.post("/books", async (req, res) => {
+  try {
+    const book = new Book(req.body.book);
+    await book.save();
+    return res.status(201).send({ book });
+  } catch (e) {
+    console.log(e);
+    if (e instanceof mongoose.Error.ValidationError) {
+      return res.status(400).send({ error: "ValidationError" });
+    } else {
+      return res.status(500).send({ error: "Internal Error" });
+    }
+  }
 });
 
 // HTTP GET /books/:id
-app.get("/books/:id", (req, res) => {
-  // ...
-  console.log(`A GET Request was made! Getting book ${req.params.id}`);
+app.get("/books/:id", async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+
+    if (!book) return res.status(404).send({ error: "Not Found" });
+
+    return res.send({ book });
+  } catch (e) {
+    if (e instanceof mongoose.Error.CastError) {
+      return res.status(400).send({ error: "Not a valid ID" });
+    } else {
+      return res.status(500).send({ error: "Internal Error" });
+    }
+  }
 });
 
 // HTTP PATCH /books/:id
-app.patch("/books/:id", (req, res) => {
-  // ...
-  console.log(`A PATCH Request was made! Updating book ${req.params.id}`);
+app.patch("/books/:id", async (req, res) => {
+  const { id } = req.params;
+  const { updates } = req.body;
+
+  try {
+    const updatedBook = await Book.findByIdAndUpdate(id, updates, {
+      runValidators: true,
+      new: true,
+    });
+
+    if (!updatedBook) return res.status(404).send({ error: "Not Found" });
+
+    return res.send({ book: updatedBook });
+  } catch (e) {
+    if (e instanceof mongoose.Error.CastError) {
+      return res.status(400).send({ error: "Not a valid ID" });
+    } else {
+      return res.status(500).send({ error: "Internal Error" });
+    }
+  }
 });
 
 // HTTP DELETE /books/:id
-app.delete("/books/:id", (req, res) => {
-  // ...
-  console.log(`A DELETE Request was made! Deleting book ${req.params.id}`);
+app.delete("/books/:id", async (req, res) => {
+  try {
+    const deletedBook = await Book.findByIdAndDelete(req.params.id);
+
+    if (!deletedBook) return res.status(404).send({ error: "Not Found" });
+
+    return res.send({ book: deletedBook });
+  } catch (e) {
+    if (e instanceof mongoose.Error.CastError) {
+      return res.status(400).send({ error: "Not a valid ID" });
+    } else {
+      return res.status(500).send({ error: "Internal Error" });
+    }
+  }
 });
 
 // Binding our application to port 3000.
